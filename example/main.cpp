7f39c9612aa24c0b10092a099da7a674f9b65a8e
Radomyr <qulowg@gmail.com>
2025-07-16 08:17:55 +0300

    fix: Переплутав < та > :).

--- example/main.cpp
+++ example/main.cpp
@@ -4,13 +4,10 @@ import jizhak;
 using namespace std::string_literals;
 
 int main() {
-    if constexpr (jzh::compiler::msvc)
-        jzh::println("Compiler: MSVC");
-    else if constexpr (jzh::compiler::clang)
-        jzh::println("Compiler: Clang");
-    else if constexpr (jzh::compiler::gcc)
-        jzh::println("Compiler: GCC");
-    else
-        jzh::println("Compiler: Unknown");
+    try {
+        jzh::print("Привіт світ!");
+    } catch (const std::system_error& e) {
+        std::cout << "Error: " << e.what() << '\n';
+    }
     return 0;
 }
