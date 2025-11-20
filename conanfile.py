ae6bd085479c276661351b45c2fef37ad46bef6e
Radomyr <qulowg@gmail.com>
2025-11-20 03:01:25 +0200

    feat: Оновлено файл conanfile.py.

--- conanfile.py
+++ conanfile.py
@@ -1,60 +1,53 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy
 
-class JizhakConan(ConanFile):
-    name = "Jizhak"
+
+class JizhakRecipe(ConanFile):
+    name = "jizhak"
     version = "1.0.0"
     license = "MIT"
-    author = "Qulow (Radomyr B.) <qulowg@gmail.com>"
+    author = "Qulow (Radomyr B.)"
     url = "https://github.com/BRamil0/Jizhak"
-    description = "Your C++ library description"
-    topics = ("modules", "cpp23", "fmt", "boost")
-    settings = "io"
-    generators = "cmake", "cmake_find_package"
-    exports_sources = "src/*", "include/*", "CMakeLists.txt"
+    description = "C++23 library with modules support"
 
-    requires = (
-        "fmt/11.2.0",
-        "boost/1.88.0"
-    )
+    settings = "os", "compiler", "build_type", "arch"
 
-    options = {
-        "shared": [True, False]
-    }
-    default_options = {
-        "shared": False,
-        "boost:shared": False,
-        "fmt:shared": False
-    }
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
 
-    _cmake = None
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
 
-    def configure(self):
-        if self.options.shared:
-            self.options["boost"].shared = True
-            self.options["fmt"].shared = True
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("fmt/[*]")
+        self.requires("boost/[*]")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_CXX_STANDARD"] = "23"
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self)
-        self._cmake.definitions["CMAKE_CXX_STANDARD"] = "23"
-        self._cmake.definitions["CMAKE_CXX_STANDARD_REQUIRED"] = "ON"
-        self._cmake.configure()
-        return self._cmake
-
     def package(self):
-        self.copy("*.hpp", dst="include", src="include")
-        self.copy("*.h", dst="include", src="include")
-        self.copy("*jizhak.lib", dst="lib", keep_path=False)
-        self.copy("*jizhak.a", dst="lib", keep_path=False)
-        self.copy("*jizhak.dll", dst="bin", keep_path=False)
-        self.copy("*jizhak.so", dst="lib", keep_path=False)
-        self.copy("*jizhak.dylib", dst="lib", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
+
+        copy(self, "*.cppm", self.source_folder, self.package_folder + "/src")
 
     def package_info(self):
-        self.cpp_info.libs = ["jizhak"]
+        self.cpp_info.libs = ["Jizhak"]
+
+        self.cpp_info.cxxflags = ["-std=c++23"]
+        if self.settings.compiler == "msvc":
+            self.cpp_info.cxxflags = ["/std:c++latest"]
